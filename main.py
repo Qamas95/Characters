a45c9a6dfbe23b6dbe17fb801b4131de96eef884
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-06-13 18:43:45 +0200

    Print data from databse * create Button to print data from database

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def addCharacter():
     #Create cursor
     cursor = dbConnection.cursor()
     #Create table
+
+    
+    #Todo: If table exist skip creating table!
+    
     cursor.execute("""CREATE TABLE characters (
                         name text
                         )""") #add in future, world, lvl
@@ -36,5 +40,34 @@ characterName.grid(row=0, column=0)
 
 submitButton = Button(root, text="Add Character",command=addCharacter)
 submitButton.grid(row=1, column=0, columnspan=2)
+
+
+#print data from databse
+
+def print():
+    #Create db and connect
+    dbConnection = sqlite3.connect('characters.db')
+    #Create cursor
+    cursor = dbConnection.cursor()
+
+    cursor.execute("SELECT *, oid FROM characters")
+    
+    records = cursor.fetchall()
+
+    #loop thru results
+    print_records = ''
+
+    for record in records:
+        print_records += str(record) + " " "\n"
+    
+    #create label to for printed records
+    printLabel = Label(root, text=print_records)
+    printLabel.grid(row=3, column =0, columnspan=2)
+
+    dbConnection.commit()
+    dbConnection.close()
+
+printData = Button(root, text = "Print data", command=print)
+printData.grid(row=2, column=0, columnspan=2)
 
 root.mainloop()
