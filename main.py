528fa7ad857e9421e225bc58675bb2fd0f752db9
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-06-13 18:26:48 +0200

    Databse connection * create database and database connection * create cursor * create table * insert into table data from entryBox

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from tkinter import *
+import sqlite3
+from tkinter import messagebox
 
 root = Tk()
 root.title("Character Databse")
 root.geometry("600x150")
 
+entryName = Entry(root)
+entryName.grid(row=0, column=1)
+
 def addCharacter():
-    return
+
+    #Create db and connect
+    dbConnection = sqlite3.connect('characters.db')
+    #Create cursor
+    cursor = dbConnection.cursor()
+    #Create table
+    cursor.execute("""CREATE TABLE characters (
+                        name text
+                        )""") #add in future, world, lvl
+    #Insert into table
+    cursor.execute("INSERT INTO characters VALUES (:name)",
+                {
+                    'name': entryName.get()
+                }
+    )
+
+    dbConnection.commit()
+    dbConnection.close()
 
 
 characterName = Label(root, text="Print character name")
 characterName.grid(row=0, column=0)
-entryName = Entry(root)
-entryName.grid(row=0, column=1)
+
 
 submitButton = Button(root, text="Add Character",command=addCharacter)
 submitButton.grid(row=1, column=0, columnspan=2)
