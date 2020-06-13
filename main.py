e389e8606686eea1920af438ad58bad06df1b537
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-06-13 18:09:54 +0200

    Create simple UI * add label * add entryBox * add Button

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ root = Tk()
 root.title("Character Databse")
 root.geometry("600x150")
 
+def addCharacter():
+    return
+
+
+characterName = Label(root, text="Print character name")
+characterName.grid(row=0, column=0)
+entryName = Entry(root)
+entryName.grid(row=0, column=1)
+
+submitButton = Button(root, text="Add Character",command=addCharacter)
+submitButton.grid(row=1, column=0, columnspan=2)
+
 root.mainloop()
