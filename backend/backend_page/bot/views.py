302eb676b8148f41b4d2beae2f3347754b8b1305
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-07-17 19:26:45 +0200

    Play with posts: * add posts

--- backend/backend_page/bot/views.py
+++ backend/backend_page/bot/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
-#from django.http import HttpResponse
+
 
 
 def home(request):
@@ -8,6 +15,26 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'bot/index.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'bot/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posed']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
